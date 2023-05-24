95173555ff52e966be3fd105b7234e73c07176ea
srbssv <user@example.com>
2023-05-24 16:10:58 +0600

    Spring config added

---
+++ src/main/java/org/lame/remembrall/ConfigAppContext.java
@@ -0,0 +1,10 @@
+package org.lame.remembrall;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ConfigAppContext {
+
+}
